8d29515013948b106136b308388bffb0e1cc16f0
Thanh Huynh <hu.thanh89@yahoo.com>
2018-10-23 19:48:41 -0700

    Merge sort and quick sort

--- algorithm/sort/merge_sort.js
+++ algorithm/sort/merge_sort.js
@@ -1,3 +1,57 @@
 //----------------------------------------------------------------------//
 // Merge Sort
+//
+// Average Time complexity - o(nlogn)
+// Worst Time complexity   - o(nlogn)
+// Space complexity        - O(n)
+//----------------------------------------------------------------------//
+
+function merge(left, right){
+
+    let result = [];
+    let lLen = left.length;
+    let rLen = right.length;
+    let l = 0;
+    let r = 0;
+    
+    while(l < lLen && r < rLen){
+        if(left[l] < right[r]){
+            result.push(left[l++]);
+        }
+        else{
+            result.push(right[r++]);
+        }
+    }  
+    
+    //remaining part needs to be added to the result
+    
+    return result.concat(left.slice(l)).concat(right.slice(r));
+}
+
+function mergeSort(arr){
+    var len = arr.length;
+    if(len <2){
+        return arr;
+    }
+
+    let mid = Math.floor(len/2);
+    let left = arr.slice(0,mid);
+    let right =arr.slice(mid);
+    
+    // send left and right to the mergeSort to broke it down into pieces
+    // then merge those
+
+    return merge(mergeSort(left),mergeSort(right));
+}
+
+//----------------------------------------------------------------------//
+// Test
+//----------------------------------------------------------------------//
+
+let test = [6, 2, 3, 5, 6, 2, 6, 7, 3, 4];
+
+let result = mergeSort(test);
+
+console.log(result);
+
 //----------------------------------------------------------------------//
--- algorithm/sort/quick_sort.js
+++ algorithm/sort/quick_sort.js
@@ -1,9 +1,83 @@
 //----------------------------------------------------------------------//
 // Quick Sort
 //
-// Functions to be implemented:
-//  - insert(data) 
-//  - remove(data) 
+// Usually the pivot is picked at random.
+// But for here, we will use the right most element
+// of the array as the pivot.
 //
-// Helper functions:
+// Average Time complexity - o(nlogn)
+// Worst Time complexity   - o(n^2)
+// Space complexity        - O(logn)
 //----------------------------------------------------------------------//
+
+// Pick a random pivot from array
+
+function pickPivot(inputs){
+    let max = inputs.length;
+    let min = 0;
+    let number = Math.random(0, inputs.length) * (max - min) + min;
+    let index  = Math.floor(number);
+    return {
+        index: index,
+        value: inputs[index]
+    }
+}
+
+// Swap index A with index B.
+
+function swap(inputs, indexA, indexB){
+    let temp = inputs[indexA];
+    inputs[indexA] = inputs[indexB];
+    inputs[indexB] = temp;
+    return inputs;
+}
+
+function partition(arr, pivot, left, right){
+
+    let pivotValue     = arr[pivot];
+    let partitionIndex = left;
+
+    console.log(arr, 'pIndex', pivot, 'left', left, 'right', right)
+
+    for(let i = left; i < right; i++){
+        if(arr[i] < pivotValue){
+            swap(arr, i, partitionIndex);
+            console.log(arr, 'pIndex', pivot, 'left', left, 'right', right, 'swap', i, partitionIndex)
+            partitionIndex++;
+        }
+    }
+    swap(arr, right, partitionIndex);
+    return partitionIndex;
+
+}
+
+function quickSort(inputs, leftIndex, rightIndex){
+
+    let pivot;
+    let partitionIndex;
+
+    if(leftIndex < rightIndex){
+        
+        pivot = rightIndex;
+
+        partitionIndex = partition(inputs, pivot, leftIndex, rightIndex)
+
+        // sort left
+        quickSort(inputs, leftIndex, partitionIndex - 1);
+
+        // sort right
+        quickSort(inputs, partitionIndex + 1, rightIndex);
+
+    }
+
+    return inputs;
+    
+}
+
+
+
+let test = [6, 2, 3, 5, 6, 2, 6, 7, 3, 4];
+
+let result = quickSort(test, 0, test.length -1);
+
+console.log(result);
